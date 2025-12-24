c4b997f2be5cc648884c6fb06ff16fb09a94b154
itdanja2025 <itdanja2025@gmail.com>
2025-12-24 18:11:50 +0900

    practice2 feed

---
+++ day02/practice2.js
@@ -0,0 +1,37 @@
+
+// 1. 숫자 찾기 
+console.log( 2000 ) // 제품별 가격 
+console.log( 4000 ) // 총결제금액
+console.log( 3 ) // 주문수량
+// vs 가독성(의미부여) 높이기 위해 변수 활용
+let 제품별가격 = 2000
+console.log( 제품별가격 )
+let 총결제금액 = 4000 
+console.log( 총결제금액 )
+let 주문수량 = 3
+console.log( 주문수량 )
+
+// 2. 문자열 찾기 
+console.log( "아메리카노" , "스무디/쉐이크" , "A-03" );
+
+// 3. 불리언 찾기
+const 품절여부 = true; console.log( 품절여부 );
+const 영수증출력여부 = false; console.log( 영수증출력여부 );
+const 적립여부 = true; console.log( 적립여부 );
+
+// 4. 배열찾기 
+let priceAry = [ 2000 , 3000 , 4000 ];
+console.log( priceAry )
+
+// 5. 객체찾기 
+let produtObj = { name : "원조커피" , price : 2500 , stock : 35 }
+console.log( produtObj )
+
+
+
+
+
+
+
+
+
