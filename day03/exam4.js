7c73536c55fe0993341773790292c4b08cae7458
itdanja2025 <itdanja2025@gmail.com>
2025-12-26 18:15:44 +0900

    20251226

--- day03/exam4.js
+++ day03/exam4.js
@@ -7,12 +7,76 @@ console.log( parseInt( 10 / 3 ) ); // 몫 3
 console.log( 10 % 3 ); // 나머지 1
 
 // [지문1] prompt 함수로 국어 , 영어 , 수학 점수를 각 입력받아서 (각 변수에 저장하고) 총점 과 평균을 계산하여 console탭에 출력하시오.
+// 소프트웨어 프로세스 : 입력 --> 저장 --> 처리 --> 출력 
+/*
+let kor = prompt("국어점수 : "); // 1. prompt 함수로 국어 , 영어 , 수학 점수를 각 입력
+let eng = prompt("영어점수 : "); // 2. 각 변수에 저장하고
+let math = prompt("수학점수 : ");
+// 3. 처리( 계산 )
+let sum = Number(kor) + Number(eng) + Number(math) ;    // * prompt 로 입력받은것들은 문자열이다.
+let avg = sum / 3 ;
+// 4. 출력
+console.log( `result1 sum : ${ sum } , avg : ${ avg }` ); // `백틱 : 문자열 변수 문자열 변수  
+*/
+
 // [지문2] prompt 함수로 반지름를 입력받아서 원넓이[반지름*반지름*3.14] 계산하여 console탭에 출력하시오.
-// [지문3] prompt 함수로 두 실수를 입력받아서 앞 실수의 값이 뒤실수의 값의 비율% 계산하여 console탭에 출력하시오.
+/*
+let radius = Number( prompt('반지름 : ' ) ); //  ( ) 안에 있는 소괄호 우선처리
+console.log( `result2 : ${ radius * radius * 3.14 } ` );
+*/
+
+// [지문3] prompt 함수로 두 실수를 입력받아서 앞 실수의 값이 뒤실수의 값의 비율(백분율)% 계산하여 console탭에 출력하시오.
+/*
+let f1 = Number( prompt('실수1 : ') ) 
+let f2 = Number( prompt('실수2 : ') )
+let ratio = ( f1 / f2 ) * 100; // 1 :100% , 0.1 : 10% , 0.01 : 1% 
+console.log( `result3 : ${ ratio }%`);
+*/
+
+
+// 2] 비교연산자 : >초과 <미만 >=이상 <=이하 ==(값)같다 !=(값)같지않다 ===(값과타입)같다 !==(값과타입)같지않다.
+// 비교 결과는 true 또는 false 
+
+// 3] 논리연산자 : && 이면서  || 이거나  ! 부정  
+// | : 엔터 위에 원화기호+shifr 
+// && 이면서 이고 면서 그리고 모두 , 모두 참이면 참 , 초등학생이 사탕이면서 초콜릿 먹을래
+// || 이거나 거나 또는 하나라도 , 하나라도 참이면 참 , 초등학생이 사탕이거나 초콜릿 먹을래
+// ! : 부정 반대 not조건 , true -> false , false -> true 
+// 주의할점 : 50 < 점수 < 100 표현식이 없다. --> 점수 > 50 && 점수 < 100 
+// 논리 결과는 true 또는 false 
 
 // [지문4] prompt 함수로 정수를 입력받아 입력받은 값이 홀수이면 true / 짝수이면 false 로 console탭에 출력하시오.
+/*
+// 4-1 : 입력 , 다음에 처리할 연산이 있으므로 변수에 저장하면 좋다.
+// 4-2 : 저장 
+let int1 = prompt('정수 : ');
+// 4-3 : 처리( 값이 홀수/짝수 ) , 값 % 2 했을때 나머지가 0 이면 짝수 , 1이면 홀수
+let result = int1 % 2 == 1 ; // 입력받은 int1 값이 나누기 2했을때 1 과 같으면 
+// 주의할점 : =(대입) , ==(비교)
+// 4-4 : 출력 
+console.log( `result4 : ${ result }`);
+*/
+
 // [지문5] prompt 함수로 정수를 입력받아 입력받은 값이 7의 배수이면 true / 아니면 false 로 console탭에 출력하시오.
+// * 배수찾기 : 값 % 배수값 했을떄 나머지가 0 이면 그 값은 배수값이다.
+/*
+let int2 = prompt('정수 : ');
+console.log( `result5 : ${ int2 % 7 == 0 }` )
+*/
+
 // [지문6] prompt 함수로 아이디 와 비밀번호를 입력받아서 (입력받은)아이디가 'admin' 이고 (입력받은)비밀번호가 1234 와 일치하면 true / 아니면 false 출력하시오.
+let id = prompt(' id : ');
+let pwd = prompt( ' pwd : ');
+// 조건2개 == 이고 == 조건1 && 조건2 
+let result = id == 'admin' && pwd == '1234' // 조건2개를 모두 만족하면 true
+console.log( `result6 : ${ result }`);
+
 // [지문7] prompt 함수로 정수를 입력받아 입력받은 값이 홀수 이면서 7배수 이면 true / 아니면 false 로 console탭에 출력하시오.
+let int3 = prompt(' 정수 : ');
+let result2 = int3 % 2 == 1 && int3 % 7 == 0
+console.log( `result7 : ${ result2 }`);
+// 주의할점 : 변수명은 같은 파일 또는 범위내 중복 이름이 안된다.
+
 // [지문8] 1차점수 와 2차점수 prompt함수로 각 입력받아서 총점이 150점이상이면 '합격' 아니면 '불합격' HTML의 <h3> 에 출력하시오.
+
 // [지문9] 두 사람의 이름을 prompt함수로 각 입력받아서 만일 이름이 '유재석' 이면 뒤에 (방장) 이라고 이름 뒤에 붙이고 아니면 생략한다.  HTML의 <ol> 에 결과를 출력하시오.
