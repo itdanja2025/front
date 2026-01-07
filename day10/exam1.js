8d23de5f71640a5931cd11f120e19d4b874433ad
itdanja2025 <itdanja2025@gmail.com>
2026-01-07 17:32:41 +0900

    20260107 7

--- day10/exam1.js
+++ day10/exam1.js
@@ -15,33 +15,48 @@ person2.email = "qwe@qwe.com";  // [1] 객체내 속성 추가 : (객체)변수�
 delete person2.age;             // [2] delete (객체)변수명.삭제할속성명;
 console.log( person2 );         // [3] 객체 확인 
 
-/* 예제 4: 객체 속성 값 계산하기
-다음 product 객체는 상품의 이름, 가격, 재고량을 나타냅니다. 이 객체의 총 자산 가치(가격 * 재고량)를 계산하여 콘솔에 출력하시오.
-const product = { name: '노트북', price: 1200000, stock: 50 }; */
+/* 예제 4: 객체 속성 값 계산하기 : 다음 product 객체는 상품의 이름, 가격, 재고량을 나타냅니다. 이 객체의 총 자산 가치(가격 * 재고량)를 계산하여 콘솔에 출력하시오. */
+const product = { name: '노트북', price: 1200000, stock: 50 };
+const total = product.price * product.stock ; // [1] 객체내 1개 속성값 호출 : (객체)변수명.속성명
+console.log( total );
 
-/* 예제 5: 대괄호 표기법 사용하기
-다음 user 객체가 있습니다. 대괄호 표기법을 사용하여 job-title 속성의 값('개발자')을 콘솔에 출력하시오.
-const user = { name: '김코딩','job-title': '개발자'}; */
+/* 예제 5: 대괄호 표기법 사용하기 : 다음 user 객체가 있습니다. 대괄호 표기법을 사용하여 job-title 속성의 값('개발자')을 콘솔에 출력하시오.*/
+const user = { name: '김코딩','job-title': '개발자'};
+// user.job-title;[x] 불가능       // [1] 객체내 속성명이 특수문자가 포함된경우 : 변수명["속성명"]
+console.log( user["job-title"] )  // [2] 접근 연산자가 아닌 [ ] 대괄호 표기법 사용한다.
 
-/*예제 6: 중첩된 객체 다루기
-다음 student 객체에서 학생이 사는 도시(city)의 이름('Seoul')을 콘솔에 출력하시오.
-const student = {  name: '이학생', age: 25, address: { city: 'Seoul', zipCode: '12345' } };*/
+/*예제 6: 중첩된 객체 다루기 : 다음 student 객체에서 학생이 사는 도시(city)의 이름('Seoul')을 콘솔에 출력하시오.*/
+const student = {  name: '이학생', age: 25, address: { city: 'Seoul', zipCode: '12345' } };
+console.log( student.address.city ) ; // [1] 객체 안에 객체가 포함될수 있다. 중첩!!
 
-/* 예제 7: 객체에 특정 속성이 있는지 확인하기(  in 연산자 )
-character 객체에 level이라는 속성이 있는지 in 연산자를 사용하여 확인하고, 결과를 true 또는 false로 콘솔에 출력하시오.
-const character = {  name: '전사', hp: 200, mp: 50 }; */
 
-/* 예제 8: Object.keys()와 Object.values()
-country 객체의 모든 키(key) 들을 배열로 만들어 출력하고, 모든 값(value) 들을 또 다른 배열로 만들어 콘솔에 각각 출력하시오.
-const country = {name: '대한민국', capital: '서울', population: 51780000 }; */
+/* 예제 7: 객체에 특정 속성이 있는지 확인하기(  in 연산자 ) : character 객체에 level이라는 속성이 있는지 in 연산자를 사용하여 확인하고, 결과를 true 또는 false로 콘솔에 출력하시오.*/
+const character = {  name: '전사', hp: 200, mp: 50 }; 
+console.log( "level" in character ); // [1] "찾을속성명" in (객체)변수명 : 존재하면 true 아니면 false 
 
-/* 예제 9: 객체와 조건문 활용하기
-다음 inventory 객체는 각 상품의 재고를 나타냅니다. for 반복문과 if 조건문을 사용하여 재고가 10개 미만인 상품의 이름을 콘솔에 출력하시오.
-const inventory = { apple: 15, banana: 5, orange: 20, grape: 8 }; */
+/* 예제 8: Object.keys()와 Object.values() : country 객체의 모든 키(key) 들을 배열로 만들어 출력하고, 모든 값(value) 들을 또 다른 배열로 만들어 콘솔에 각각 출력하시오. */
+const country = {name: '대한민국', capital: '서울', population: 51780000 };
+console.log( Object.keys( country ) );      // [1] Ojbect.keys( 객체변수명 ) : 객체내 모든 키 반환 
+console.log( Object.values( country ) );    // [2] Object.values( 객체변수명 ) : 객체내 모든 값 반환
 
-/* 예제 10: 객체 배열 순회하기
-다음은 여러 학생의 정보를 담은 students 배열입니다. for 반복문을 사용하여 각 학생의 이름과 전공을 "이름: 전공" 형태로 콘솔에 출력하시오.  
-const students = [ { name: '김철수', major: '컴퓨터공학' },  { name: '이영희', major: '경영학' },  { name: '박지성', major: '체육교육' } ];*/
+/* 예제 9: 객체와 조건문 활용하기 : 다음 inventory 객체는 각 상품의 재고를 나타냅니다. for 반복문과 if 조건문을 사용하여 재고가 10개 미만인 상품의 이름을 콘솔에 출력하시오.*/
+const inventory = { apple: 15, banana: 5, orange: 20, grape: 8 };
+// [1] key 이용한 value 알수 있지만 , value 이용한 key 알수 없다. 즉] key(속성명) 중복값이 없고 value(값) 중복허용
+const keys = Object.keys( inventory );
+for( let index = 0 ; index <= keys.length-1 ; index++ ){ // [2] 모든 key를 반복문으로 순회한다.
+    //console.log( keys ); // 모든 키(속성명) 목록      //console.log( keys[index] ); // index번째 키 
+    if( inventory[ keys[index] ] < 10 ){    // [3] 만약에 index번째 재고가 10보다 작으면/미만 
+        console.log( keys[index] ); // [4] index번째 key(속성명)값 출력하기.
+    } // if end 
+} //for end 
+
+/* 예제 10: 객체 배열 순회하기 : 다음은 여러 학생의 정보를 담은 students 배열입니다. for 반복문을 사용하여 각 학생의 이름과 전공을 "이름: 전공" 형태로 콘솔에 출력하시오.  */
+const students = [ { name: '김철수', major: '컴퓨터공학' },  { name: '이영희', major: '경영학' },  { name: '박지성', major: '체육교육' } ];
+for( let index = 0 ; index <= students.length - 1 ; index++ ) {// [1] 배열 순회 : 0부터 마지막인덱스까지 
+    const student = students[index]; // index번째 학생객체 호출 
+    console.log( `${ student.name } : ${ student.major } ` );
+}  // for end 
+
 
 /* 예제 11: 공공데이터 포털 : 인천 부평구 인구 현황 테이블 만들기
 [구현 조건]
